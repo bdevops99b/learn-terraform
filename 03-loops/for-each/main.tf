55eea6596b207e22ae2915d57047a6764d72109a
bdevops99 <bdevops99@gmail.com>
2023-05-04 17:02:33 +0530

    terraform all roboshop instances

---
+++ 03-loops/for-each/main.tf
@@ -0,0 +1,35 @@
+/*
+
+resource "null_resource" "fruits" {
+  count = length(var.fruits)
+  provisioner "local-exec" {
+    command = " echo Fruit Name - ${var.fruits[count.index]} "
+  }
+}
+variable "fruits" {
+  default = [ "mango", "banana", "orange" ]
+}*/
+
+resource "null_resource" "fruits1" {
+  for_each = var.fruits1
+  provisioner "local-exec" {
+    command = "echo fruit name - ${each.key} - ${each.value["count"]}"
+  }
+}
+
+variable "fruits1" {
+  default = {
+    pom={
+      name="pom"
+      count=10
+    }
+    mango={
+      name="mango"
+      count=200
+    }
+    watermelon={
+      name="watermelon"
+      count=500
+    }
+  }
+}
