dc387edb9a88ca449374497e2b8292045cdb952d
bdevops99 <bdevops99@gmail.com>
2023-05-04 14:54:09 +0530

    terraform all roboshop instances

--- 03-loops/main.tf
+++ 03-loops/main.tf
@@ -5,9 +5,9 @@ resource "null_resource" "null" {
 resource "null_resource" "fruits" {
   count=length(var.fruits)
   provisioner "local-exec" {
-    command = "echo Fruit Name - ${var.fruits[count.index]}"
+    command = " echo Fruit Name - ${var.fruits[count.index]} "
   }
 }
 variable "fruits" {
-  default = [ "mango", "banana", "orange"]
+  default = [ "mango", "banana", "orange" ]
 }
