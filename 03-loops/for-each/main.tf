fdd17b81253a15a6f84ad1d0bd30764bb2091572
bdevops99 <bdevops99@gmail.com>
2023-05-04 17:07:02 +0530

    terraform all roboshop instances

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -13,8 +13,8 @@ variable "fruits" {
 resource "null_resource" "fruits1" {
   for_each = var.fruits1
   provisioner "local-exec" {
-   # command = "echo fruit name - ${each.key} - ${each.value["count"]}"
-    command = "echo fruit name - ${each.value["name"]} - ${each.value["count"]}"
+    command = "echo fruit name - ${each.key} - ${each.value["count"]}"
+    #command = "echo fruit name - ${each.value["name"]} - ${each.value["count"]}"
   }
 }
 
