36dabaf43b4b76bfa38ab10721617ac9035ec4be
bdevops99 <bdevops99@gmail.com>
2023-05-04 10:21:49 +0530

    terraform all roboshop instances

--- 01-vars/output.tf
+++ 01-vars/output.tf
@@ -29,3 +29,6 @@ output "env" {
 output "auto_num1" {
   value = var.auto_num1
 }
+output "sample1" {
+  value = var.sample1
+}
--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -41,3 +41,4 @@ variable "sample_dict" {
 
 variable "env" {}
 variable "auto_num1" {}
+variable "sample1" {}
