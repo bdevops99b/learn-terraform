d035550cdd40aea262f705b4152c58a419361792
bdevops99 <bdevops99@gmail.com>
2023-05-06 12:35:17 +0530

    terraform all roboshop instances

--- 04-functions/mains.tf
+++ 04-functions/mains.tf
@@ -6,3 +6,24 @@ variable "sample" {
 output "sample" {
   value = upper(var.sample)
 }
+
+variable "sample1" {
+  default = ["lkg","ukg"]
+}
+
+variable "sample2" {
+  default = {
+    xyz=100
+    abc=200
+  }
+}
+
+#print first value like var.sample1[0] with a function
+
+output "sample1" {
+  value = element(var.sample1,0)
+}
+
+output "sample2" {
+  value = lookup(var.sample2,"a1" )
+}
