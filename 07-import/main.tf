4d4dc1cb362931d4d11f8b222199f50bbacb39de
bdevops99 <bdevops99@gmail.com>
2023-05-07 17:33:18 +0530

    terraform all roboshop instances

--- 07-import/main.tf
+++ 07-import/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "workstaion" {
-  ami                                  = "ami-0b5a2b5b8f2be4ec2"
-  instance_type                        = "t3.micro"
-  tags                                 = {
-     "Name" = "workstation"
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+  tags          = {
+    "Name" = "workstation"
   }
-output "private_ip" {
-  value = aws_instance.workstaion.private_ip
+  output "private_ip" {
+    value = aws_instance.workstaion.private_ip
+  }
 }
